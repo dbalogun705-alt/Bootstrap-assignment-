6d0d487fb3575290226298df58fae844381a8835
dbalogun705-alt <dbalogun705@gmail.com>
2026-07-05 18:28:59 +0100

    new file

---
+++ task.js
@@ -0,0 +1,25 @@
+let Name="Ayo"
+console.log (Name)
+
+const Age="18"
+if (Age>=18){
+    console.log("Adult")
+}
+else{console.log("Teenager")}
+console.log(Age)
+
+let task="Complete the task"
+let result=50
+isfaulty=true
+console.log(task)
+console.log(result)
+console.log(isfaulty)
+
+for ( j=1, j<=7; j++;){
+    console.log("Number"=j);
+}
+
+document.getElementById('btn') 
+.addEventListener{'Create Task', {} => 
+alert ("Welcome ");
+};
